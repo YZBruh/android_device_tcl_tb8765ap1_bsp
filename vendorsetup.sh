3769de24012201524d35cf9b220e2de68baf0d10
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-11-04 16:20:27 +0300

    twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_tb8765ap1_bsp-user
-add_lunch_combo omni_tb8765ap1_bsp-userdebug
-add_lunch_combo omni_tb8765ap1_bsp-eng
+add_lunch_combo twrp_tb8765ap1_bsp-user
+add_lunch_combo twrp_tb8765ap1_bsp-userdebug
+add_lunch_combo twrp_tb8765ap1_bsp-eng
