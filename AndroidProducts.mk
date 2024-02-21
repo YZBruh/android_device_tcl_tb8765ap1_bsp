5c21f232a592cf5972e5d6f627bf4aedff9dc9cc
YZBruh <yagizzengin73@gmail.com>
2024-02-22 00:00:49 +0300

    tb8765ap1_bsp: add more description to the code

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_tb8765ap1_bsp.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_tb8765ap1_bsp-eng
+    omni_tb8765ap1_bsp-eng # just having it in eng is enough. no need for more
