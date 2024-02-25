142e56384fcf36f61275e3d528496607ebb686b3
YZBruh <yagizzengin73@gmail.com>
2024-02-25 22:17:45 +0300

    tb8765ap1_bsp: a few important things...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,8 @@ BOARD_USES_MTK_HARDWARE := true
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system_root/system/etc/recovery.fstab
+TARGET_TWRP_FLAGS := $(DEVICE_PATH)/recovery/root/system_root/system/etc/twrp.flags
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Assert
@@ -92,6 +93,7 @@ RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_DEFAULT_LANGUAGE := en
+TW_NO_SCREEN_BLANK := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
