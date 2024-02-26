686b92b5a55dfdadc816342b9ba6e562ffd0512d
YZBruh <yagizzengin73@gmail.com>
2024-02-26 18:30:22 +0300

    tb8765ap1_bsp: make sure properties are added to default.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,7 @@ BOARD_USES_MTK_HARDWARE := true
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/properties/system.prop
 
 # Copy the twrp.flags into recovery image
 PRODUCT_COPY_FILES += \
